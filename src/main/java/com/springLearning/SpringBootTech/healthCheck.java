dbb13c67a6a3cd69b630d1c39204573b6b63b5d2
Sagar Tikeshwar Turkar <sagar.turkar@vayana.com>
2026-05-28 13:04:48 +0530

    Added first health check mapping using  anotation @RestController and @GetMapping

---
+++ src/main/java/com/springLearning/SpringBootTech/healthCheck.java
@@ -0,0 +1,14 @@
+package com.springLearning.SpringBootTech;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class healthCheck {
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+
+}
